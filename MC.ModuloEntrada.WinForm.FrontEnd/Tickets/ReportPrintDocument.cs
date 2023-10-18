e2c6c2e8245a215f9dcd26eb1d1a51c921ec9f75
Elejota <luiscarloslealjimenez@hotmail.com>
2023-10-18 16:18:13 -0500

    Mas reporting

--- MC.ModuloEntrada.WinForm.FrontEnd/Tickets/ReportPrintDocument.cs
+++ MC.ModuloEntrada.WinForm.FrontEnd/Tickets/ReportPrintDocument.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Reporting.WinForms;
+﻿using Microsoft.Reporting.WebForms;
+using Microsoft.Reporting.WinForms;
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
